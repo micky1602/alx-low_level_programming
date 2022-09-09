5c41031968530963ddcfc92fa382c6a8a1b76dfc
micky1602 <chukwu.n.michael@gmail.com>
2022-09-08 19:44:29 -0700

    message 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
-* main - print the string in theput fuction 
+* main - print the string in the put function 
 *
-* Discription: usuing the main fuction 
+* Description: using the main function 
 * this program prints "Programming is like building a multilingual puzzle
 * Return: 0
 */
