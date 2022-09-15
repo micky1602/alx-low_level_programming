04c5771efcd429a6ef76c431608bc5b98fd4f909
micky1602 <chukwu.n.michael@gmail.com>
2022-09-15 09:50:01 -0700

    message8

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
 #include "main.h"
-
 /**
- * main - print putchar
+ * main - Entry point
  * Return: Always returns 0
  */
+
 int main(void)
 {
 	_putchar('_');
@@ -15,5 +15,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
